a33db49f4411c9755c6490256840b0fadcf1ce67
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2022-10-26 11:53:39 +0200

    exploded war

--- maven-war-exploded/webapp/src/main/java/com/example/demo/controller/MainController.java
+++ maven-war-exploded/webapp/src/main/java/com/example/demo/controller/MainController.java
@@ -1,14 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.service.HelloService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-    @Autowired
-    private HelloService helloService;
+    private final HelloService helloService = new HelloService();
     @GetMapping("")
     public String hello() {
         return helloService.hello();
